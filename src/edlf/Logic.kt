c710fa5d6283427d1367200964f50533732e5269
Michele d'Amico <michele.damico@gmail.com>
2016-02-25 23:57:27 +0100

    Base

--- src/edlf/Logic.kt
+++ src/edlf/Logic.kt
@@ -1,7 +1,10 @@
 package edlf
 
+import graph.*
+import kotlin.reflect.jvm.internal.impl.javax.inject.Inject
+
 /**
- * Logic is our logic schema. Provide methods to create logic componets and
+ * Logic is our logic schema. Provide methods to create logic components and
  * inputs nodes where to post new changes. Every components can define one or more
  * outputs points.
  * All inputs and outputs point have
@@ -16,6 +19,238 @@ package edlf
  * after build it. The logic cannot change after first event (it is not dynamic).
  */
 
-class Logic(){
+
+open class Components<T>() {
+    var outEvents = ChangeEvents<T>()
+    internal var g = DAG<Component<T>>()
+
+    fun process(changedNode: Input<T>) {
+        ProcessAlgorithm(changedNode).process()
+    }
+
+    open fun input(initialValue: T) = Input(this, initialValue)
+    open fun output(input: Component<T>) = Output(this, input)
+}
+
+class Logic() : Components<Boolean>() {
+
+    fun input() = super.input(false)
+
+    fun not(input: Component<Boolean>) = Not(this, input)
+
+    fun xor(input0: Component<Boolean>, input1: Component<Boolean>) = Xor(this, input0, input1)
+
+    fun nand(input0: Component<Boolean>, input1: Component<Boolean>) = Nand(this, input0, input1)
+
+}
+
+class ProcessAlgorithm<T>(changedNode: Input<T>){
+    private var toProcess = mutableListOf<Component<T>>(changedNode)
+    var sourcesMap = sourcesMap(changedNode)
+
+    fun process() {
+        while (toProcess.isNotEmpty()) {
+            var node = toProcess.removeAt(0)
+            toProcess.addAll(node.processChanged(this))
+        }
+    }
+
+    fun processWire(source: Component<T>, destination: Component<T>) {
+        sourcesMap[destination]!!.remove(source)
+    }
+
+    fun processableNode(node: Component<T>): Boolean {
+        return sourcesMap[node]!!.isEmpty()
+    }
+
+    fun processAllWire(node: Component<T>) {
+        node.allWires().map {
+            pair ->
+            var (source, dest) = pair
+            processWire(source, dest)
+        }
+    }
+
+    private fun sourcesMap(node: Component<T>) : MutableMap<Component<T>, MutableSet<Component<T>>> {
+        var sourcesMap = mutableMapOf<Component<T>, MutableSet<Component<T>>>()
+        node.allWires().map {
+            wire ->
+            var (source, dest) = wire
+            if (dest !in sourcesMap) {
+                sourcesMap.put(dest, mutableSetOf<Component<T>>())
+            }
+            sourcesMap[dest]!!.add(source)
+        }
+        return sourcesMap
+    }
+
+}
+
+abstract class Component<T>(var owner: Components<T>, inputs: List<Component<T>>) {
+    protected val inputs = inputs
+    private var inputStates = getInputsStates()
+    protected val node = owner.g.addNode(this)
+
+    init {
+        inputs.forEach { owner.g.addEdge(it, this) }
+    }
+
+    constructor(owner: Components<T>, vararg inputs: Component<T>) : this(owner, inputs.asList())
+
+    protected var state = logic()
+
+    abstract fun logic(): T
+
+    fun value(): T {
+        return state
+    }
+
+    fun dependedComponents(): Collection<Component<T>>{
+        return node.outputs()
+    }
+
+    fun allWires(): Collection<Pair<Component<T>,Component<T>>>{
+        return node.bfsEdges()
+    }
+
+    open internal fun processChanged(algorithm: ProcessAlgorithm<T>):
+            Collection<Component<T>>{
+        var changedComponents = mutableListOf<Component<T>>()
+        for (child in dependedComponents()){
+            algorithm.processWire(this, child)
+            if (algorithm.processableNode(child)) {
+                if (child.update()){
+                    changedComponents.add(child)
+                }else{
+                    algorithm.processAllWire(child)
+                }
+            }
+        }
+        return changedComponents
+    }
+
+    private fun getInputsStates() = inputs.map { it.value() }
+
+    protected fun compute() {
+        if (!stateChanged()) {
+            throw RuntimeException("Call compute without change source")
+        }
+        stateUpdate()
+        state = logic()
+    }
+
+    internal fun update(): Boolean {
+        var old = state
+        compute()
+        return old != state
+    }
+
+    private fun stateChanged(): Boolean {
+        if (inputStates.size == 0) {
+            return true
+        }
+        var newStates = getInputsStates()
+        for (i in 0..(inputStates.size - 1)) {
+            if (newStates[i] != inputStates[i]) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun stateUpdate() {
+        inputStates = getInputsStates()
+    }
+}
+
+class Input<T>(owner: Components<T>, initialState: T) : Component<T>(owner) {
+    init {
+        state = initialState
+    }
+
+    override fun logic(): T {
+        return newState
+    }
+
+    private var newState = initialState
+
+    fun set(value: T) {
+        newState = value
+        if (update()) {
+            owner.process(this)
+        }
+    }
+}
+
+
+class Output<T>(owner: Components<T>, source: Component<T>) : Component<T>(owner, source) {
+    val source: Component<T>
+        get() = inputs[0]
+
+    override fun logic(): T {
+        return source.value()
+    }
+
+    override fun processChanged(algorithm: ProcessAlgorithm<T>):
+            Collection<Component<T>>{
+        owner.outEvents.push(this)
+        return emptyList()
+    }
+}
+
+class Not(owner: Logic, source: Component<Boolean>) : Component<Boolean>(owner, source) {
+    val source: Component<Boolean>
+        get() = inputs[0]
+
+    override fun logic(): Boolean {
+        return !source.value()
+    }
+
+}
+
+class Xor(owner: Logic, inputs0: Component<Boolean>, input1: Component<Boolean>) :
+        Component<Boolean>(owner, inputs0, input1) {
+    val pin0: Component<Boolean>
+        get() = inputs[0]
+    val pin1: Component<Boolean>
+        get() = inputs[1]
+
+    override fun logic(): Boolean {
+        return pin0.value() != pin1.value()
+    }
+
+}
+
+class Nand(owner: Logic, inputs0: Component<Boolean>, input1: Component<Boolean>) :
+        Component<Boolean>(owner, inputs0, input1) {
+    val pin0: Component<Boolean>
+        get() = inputs[0]
+    val pin1: Component<Boolean>
+        get() = inputs[1]
+
+    override fun logic(): Boolean {
+        return !(pin0.value() && pin1.value())
+    }
+
+}
+
+class ChangeEvents<T>() {
+    private val queue = mutableListOf<Event<T>>()
+
+    class Event<T>(var node: Output<T>, var value: T)
+
+    fun pop(): ChangeEvents.Event<T> {
+        var ret = queue.first()
+        queue.removeAt(0)
+        return ret
+    }
+
+    fun isEmpty(): Boolean {
+        return this.queue.isEmpty()
+    }
+
+    internal fun push(node: Output<T>) {
+        queue.add(Event<T>(node, node.value()))
+    }
 
 }
--- test/edlf/logic.kt
+++ test/edlf/logic.kt
@@ -1,16 +1,167 @@
+/**
+ * Test case for simple logic ports
+ *
+ * Created by michele on 15/02/16.
+ */
+
 package edlf
 
 import kotlin.test.*
 import org.junit.*
 
 class TestLogic {
+    val logic = Logic()
+    val input = logic.input()
+    val output = logic.output(input)
+    val queue = logic.outEvents
+
+    fun assertEvent(event: ChangeEvents.Event<Boolean>, output: Output<Boolean>,
+                    value: Boolean) {
+        assertEquals(output, event.node)
+        assertEquals(value, event.value)
+    }
+
     @Test
     fun base() {
-        var logic = Logic()
+        with(queue) {
+            assert(isEmpty())
+            input.set(true)
+            assertFalse { isEmpty() }
+            var event = pop()
+            assertTrue { isEmpty() }
+            assertEvent(event, output, true)
+        }
+    }
 
+    @Test
+    fun onOff() {
+        with(queue) {
+            input.set(true)
+            input.set(false)
+            assertEvent(queue.pop(), output, true)
+            assertEvent(queue.pop(), output, false)
+            assertEmpty()
+        }
+    }
+
+    @Test
+    fun noChange() {
+        with(queue) {
+            input.set(false)
+            assertEmpty()
+        }
+    }
+
+    @Test
+    fun not() {
+        val input = logic.input()
+        val not = logic.not(input)
+        val output = logic.output(not)
+        with(queue) {
+            input.set(true)
+            input.set(false)
+            assertEvent(queue.pop(), output, false)
+            assertEvent(queue.pop(), output, true)
+            assertEmpty()
+        }
+    }
+
+    @Test
+    fun xor() {
+        val input0 = logic.input()
+        val input1 = logic.input()
+        val xor = logic.xor(input0, input1)
+        val output = logic.output(xor)
+        with(queue) {
+            input0.set(true)
+            input1.set(true)
+            input0.set(false)
+            input1.set(false)
+            assertEvent(queue.pop(), output, true)
+            assertEvent(queue.pop(), output, false)
+            assertEvent(queue.pop(), output, true)
+            assertEvent(queue.pop(), output, false)
+            assertEmpty()
+        }
+    }
+
+    @Test
+    fun nand() {
+        val input0 = logic.input()
+        val input1 = logic.input()
+        val nand = logic.nand(input0, input1)
+        val output = logic.output(nand)
+        with(queue) {
+            input0.set(true)
+            assertEmpty()
+            input1.set(true)
+            assertEvent(queue.pop(), output, false)
+            input0.set(false)
+            assertEvent(queue.pop(), output, true)
+            input1.set(false)
+            assertEmpty()
+        }
+    }
+
+    @Test
+    fun atomic() {
+        val input = logic.input()
+        val nand = logic.nand(input, input)
+        val nand2 = logic.nand(input, input)
+        val xor = logic.xor(nand, nand2)
+        logic.output(xor)
+        with(queue) {
+            input.set(true)
+            input.set(false)
+            input.set(true)
+            assertEmpty()
+        }
+    }
+
+    fun assertEmpty() {
+        assert(queue.isEmpty())
+    }
+
+    @Test
+    fun notAsNand() {
+        val input = logic.input()
+        val nand = logic.nand(input, input)
+        val output = logic.output(nand)
+        with(queue) {
+            input.set(true)
+            assertEvent(pop(), output, false)
+            input.set(false)
+            assertEvent(pop(), output, true)
+            assertEmpty()
+        }
+    }
+
+    @Test
+    fun callComponentComputeJustOnce() {
+        class FakeComponent(owner: Logic, input: Component<Boolean>) :
+                Component<Boolean>(owner, input) {
+            val input: Component<Boolean>
+                get() = inputs[0]
+
+            override fun logic(): Boolean {
+                return input.value()
+            }
+
+            fun callCompute() {
+                compute()
+            }
+
+            fun changeInputState(v: Boolean) {
+                input.state = v
+            }
+        }
+
+        val fc = FakeComponent(logic, input)
+        input.set(false)
+        assertFails {
+            fc.callCompute()
+        }
+        fc.changeInputState(true)
+        fc.callCompute()
     }
 }
-
-/**
- * Created by michele on 15/02/16.
- */
